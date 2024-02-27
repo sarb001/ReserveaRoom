40432216aa478903ec90a752b4136583d82f7f17
SARB <mrsinghbusiness05@gmail.com>
2024-02-27 19:00:23 +0530

    register ui completed

--- frontend/src/components/Register/Register.jsx
+++ frontend/src/components/Register/Register.jsx
@@ -6,32 +6,53 @@ const Register = () => {
 
 
   return (
-    <div>
-         <div>
+    <div className='bg-blue-400 m-8'>
+         <div className=' w-full p-8   bg-lime-500 text-xl'>
+
             <div className="account"> 
-             <span> Create an Account </span>
+             <span className='text-2xl'> Create an Account </span>
             </div>
-            <label> Your email </label>
-            <input type = "email"  placeholder='name@company.com' />
-            <label> First Name </label>
-            <input type = "text"  placeholder='John'  required/>
 
-            <label> Last Name </label>
-            <input type = "text"  placeholder='Doe' required/>
-            
-            <label> Password </label>
-            <input type = "email"  placeholder='name@company.com'  required/>
-         
-            <input type = "checkbox"  />
-            <span> I accept the Terms and Conditions </span>
+            <div className='grid'>
+              
+                <div className='my-2'>
+                  <label> Your email </label>
+                  <input type = "email"  placeholder='name@company.com' />
+                </div>
 
-            <button> Create an Account </button>
+                <div className='my-2'>
+                  <label> First Name </label>
+                  <input type = "text"  placeholder='John'  required/>
+                </div>
 
-             <div>
-               <span> Already have an account?
-                <a href = "/login"> Login Here </a>  
-               </span>
-             </div>
+                <div className='my-2'>
+                <label> Last Name </label>
+                <input type = "text"  placeholder='Doe' required/>
+                </div>
+
+                <div className='my-2'>
+                <label> Password </label>
+                <input type = "email"  placeholder='name@company.com'  required/>
+                </div>
+
+                <div className='my-2'>
+                <input type = "checkbox"  required/>
+                <span className='text-[18px]'> I accept the Terms and Conditions </span>
+                </div>
+           
+                <div>
+                  <button className='p-2 bg-slate-900 text-white'> 
+                    Create an Account 
+                  </button>
+                </div>
+
+                <div>
+                  <span className='text-[18px]'> Already have an account?
+                    <a href = "/login"> Login Here </a>  
+                  </span>
+                </div>
+
+           </div>
 
          </div>
     </div>
